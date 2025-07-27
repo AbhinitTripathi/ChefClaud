5fb69fc1073a114d8ecb3448e13113a400fc133d
Abhinit Tripathi <114798873+AbhinitTripathi@users.noreply.github.com>
2025-07-27 06:30:48 +0000

    Hugging face Api integrated

--- src/components/ai.js
+++ src/components/ai.js
@@ -5,14 +5,10 @@ const SYSTEM_PROMPT = `
 You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page.
 `;
 
+const client = new InferenceClient(import.meta.env.VITE_HUGGING_FACE_API_KEY);
+
 export async function getRecipeFromAI(ingredientsList) {
   const ingredientsString = ingredientsList.join(", ");
-  
-  console.log("API Key:", import.meta.env.VITE_HUGGING_FACE_API_KEY);
-  
-  const client = new InferenceClient({
-    apiKey: import.meta.env.VITE_HUGGING_FACE_API_KEY
-  });
 
   try {
     const response = await client.chatCompletion({
